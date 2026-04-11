44dee70a805c0cffb43d16db586d14a007deec28
andriy-zh-ua <semenyuknataliya@gmail.com>
2026-04-11 18:10:34 -0400

    Delete animation.ts

--- src/config/animation.ts
+++
@@ -1,7 +0,0 @@
-// Animation configuration
-export const animationConfig = {
-  pulseInterval: 500, // ms between pulses
-  pulseDuration: 2000, // ms for pulse to reach partition
-};
-
-export type AnimationConfig = typeof animationConfig;
